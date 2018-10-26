323c9dc54247d0f3e0352f3dc265014089b843a2
Rico Sonntag <mail@ricosonntag.de>
2018-10-26 22:54:23 +0200

    Update Cp850Encoding.php (#8)
    Removed not required "implements"

--- src/Encodings/Cp850Encoding.php
+++ src/Encodings/Cp850Encoding.php
@@ -10,6 +10,6 @@ namespace Fisharebest\LibGedcom\Encodings;
 /**
  * Convert between UTF-8 and Windows Code Page 850.
  */
-class Cp850Encoding extends AbstractEncodingIconv implements EncodingInterface {
+class Cp850Encoding extends AbstractEncodingIconv {
     const ENCODING_NAME = 'CP850';
 }
